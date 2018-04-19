03bc82e46ee162d8e173588b98ba9d12fc637693
DanielHauge <Animcuil@gmail.com>
2018-04-20 00:44:29 +0200

    forgot the volumes had changed to /c/Users/Animc/desktop/Repos/DBEX9

--- RunNeo4JWithData.sh
+++ RunNeo4JWithData.sh
@@ -12,7 +12,7 @@ sudo sed -i -E '1s/.*/:START_ID,:END_ID/' import/social_network_edges.csv
 sudo wget -P $(pwd)/plugins https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.3.0.1/apoc-3.3.0.1-all.jar
 sudo wget -P $(pwd)/plugins https://github.com/neo4j-contrib/neo4j-graph-algorithms/releases/download/3.3.2.0/graph-algorithms-algo-3.3.2.0.jar
 
-sudo docker run -d --name neo4j --rm --publish=7474:7474 --publish=7687:7687 --volume=/root/import:/import --volume=/root/plugins:/plugins --env NEO4J_dbms_memory_pagecache_size=6G --env NEO4J_dbms_memory_heap_max_size=10G --env NEO4J_AUTH=neo4j/class --env=NEO4J_dbms_security_procedures_unrestricted=apoc.\\\*,algo.\\\* neo4j
+sudo docker run -d --name neo4j --rm --publish=7474:7474 --publish=7687:7687 --volume=$(pwd)/import:/import --volume=$(pwd)/plugins:/plugins --env NEO4J_dbms_memory_pagecache_size=6G --env NEO4J_dbms_memory_heap_max_size=10G --env NEO4J_AUTH=neo4j/class --env=NEO4J_dbms_security_procedures_unrestricted=apoc.\\\*,algo.\\\* neo4j
 sudo docker exec neo4j sh -c 'neo4j stop'
 sudo docker exec neo4j sh -c 'rm -rf data/databases/graph.db'
 sudo docker exec neo4j sh -c 'neo4j-admin import \
