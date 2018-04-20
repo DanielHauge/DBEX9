6e7e6a85bcb1931bfcfe0b90b1058cb2fae3399c
DanielHauge <Animcuil@gmail.com>
2018-04-20 03:39:13 +0200

    Update RunNeo4JWithDatas.sh

--- RunNeo4JWithDatas.sh
+++ RunNeo4JWithDatas.sh
@@ -17,7 +17,7 @@ sudo docker run -d --name neo4j --publish=7474:7474 --publish=7687:7687 -v $(pwd
 sudo docker exec neo4j sh -c 'neo4j stop'
 sudo docker exec neo4j sh -c 'rm -rf /var/lib/neo4j/data/databases/graph.db'
 sudo docker exec neo4j sh -c 'rm -rf data/databases/graph.db'
-sudo docker exec -it neo4j sh -c 'neo4j-admin import --nodes:Person import/social_network_nodes.csv --relationships:ENDORSES import/social_network_edges.csv --ignore-missing-nodes=true --ignore-duplicate-nodes=true --id-type=INTEGER'
+sudo docker exec -t neo4j sh -c 'neo4j-admin import --nodes:Person import/social_network_nodes.csv --relationships:ENDORSES import/social_network_edges.csv --ignore-missing-nodes=true --ignore-duplicate-nodes=true --id-type=INTEGER'
 sudo docker restart neo4j	  
 
 	
