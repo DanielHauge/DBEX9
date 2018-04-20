f28ea17531467f8f0b42ad3b9497018265a345ab
DanielHauge <Animcuil@gmail.com>
2018-04-20 03:46:42 +0200

    Update RunNeo4JWithDatas.sh

--- RunNeo4JWithDatas.sh
+++ RunNeo4JWithDatas.sh
@@ -14,10 +14,6 @@ sudo wget -P $(pwd)/plugins https://github.com/neo4j-contrib/neo4j-apoc-procedur
 sudo wget -P $(pwd)/plugins https://github.com/neo4j-contrib/neo4j-graph-algorithms/releases/download/3.3.2.0/graph-algorithms-algo-3.3.2.0.jar
 
 sudo docker run -d --name neo4j --publish=7474:7474 --publish=7687:7687 -v $(pwd)/import:/var/lib/neo4j/import -v $(pwd)/plugins:/var/lib/neo4j//plugins --env NEO4J_dbms_memory_pagecache_size=6G --env=NEO4J_dbms_memory_heap_max__size=10G --env NEO4J_AUTH=neo4j/class --env=NEO4J_dbms_security_procedures_unrestricted=apoc.\\\*,algo.\\\* neo4j
-sudo docker exec neo4j sh -c 'neo4j stop'
-sudo docker exec neo4j sh -c 'rm -rf /var/lib/neo4j/data/databases/graph.db'
-sudo docker exec neo4j sh -c 'rm -rf data/databases/graph.db'
-sudo docker exec -t neo4j sh -c 'neo4j-admin import --nodes:Person import/social_network_nodes.csv --relationships:ENDORSES import/social_network_edges.csv --ignore-missing-nodes=true --ignore-duplicate-nodes=true --id-type=INTEGER'
-sudo docker restart neo4j	  
+	  
 
 	
